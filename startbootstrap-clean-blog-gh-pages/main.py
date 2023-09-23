bdbd18749e71254627af6e7e1cdf8ecc644138c1
men_l <men_long@yahoo.com>
2023-09-23 16:30:17 -0400

    Initial commit

---
+++ startbootstrap-clean-blog-gh-pages/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import requests
+
+#USE YOUR OWN npoint LINK! OTHERWISE IT WILL NOT WORK 👇
+posts = requests.get("https://api.npoint.io/dd8463a897d2f65407d7").json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
