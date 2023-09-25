323c3ed04bb60f2f943d486c603bd59fb8777798
longmen2022 <98563918+longmen2022@users.noreply.github.com>
2023-09-25 15:59:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -28,5 +28,5 @@ def about():
 def contact():
     return render_template("contact.html")
 #
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
