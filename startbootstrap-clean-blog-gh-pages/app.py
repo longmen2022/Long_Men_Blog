0617a583a4ded66bc21f28c3795bb4be27409137
men_l <men_long@yahoo.com>
2023-09-25 12:35:37 -0400

    change

--- startbootstrap-clean-blog-gh-pages/app.py
+++ startbootstrap-clean-blog-gh-pages/app.py
@@ -21,5 +21,5 @@ def about():
 def contact():
     return render_template("contact.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
